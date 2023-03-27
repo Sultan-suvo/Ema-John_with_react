bf2b95554555bb49c8485a78e2d58090f181e64d
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-03-27 17:28:26 +0600

    product added

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [products , useProducts] = useState([]);
-    useEffect(()=> {
+    const [products, useProducts] = useState([]);
+    useEffect(() => {
         fetch('products.json')
-        .then(res=> res.json())
-        .then(data => useProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => useProducts(data))
+    }, [])
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                cdv
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 sdc
