f40d3d63dc206c82509612f292dc60c2df2a6eda
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-03-28 21:15:27 +0600

    show data from cart

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -3,12 +3,14 @@ import './Cart.css'
 
 const Cart = ({ cart }) => {
 
-
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity
     }
 
     const tax = totalPrice * 7 / 100;
@@ -18,7 +20,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Slected Items : {cart.length}</p>
+            <p>Slected Items : {quantity}</p>
             <p>Total Price : ${totalPrice}</p>
             <p>Total Shipping : {totalShipping}</p>
             <p>Tax : {tax.toFixed(2)}</p>
